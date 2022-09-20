1c26d93dd47f634cbab45caa6f674f9c1653cf8a
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-20 12:19:17 -0700

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,7 +11,6 @@ exec {'Install_nginx':
 exec {'Configure_redirect':
   provider => shell,
   command  => 'echo "Hello World" > /var/www/html/index.nginx-debian.html ;\
-               sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=NdYWuo9OFAw permanent;/" /etc/nginx/sites-available/default ;\
+               sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ;\
                sudo service nginx restart',
 }
-
