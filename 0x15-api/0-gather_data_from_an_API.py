fc44d636011fc635737444b560882bd97319eed8
Olumide Micheal <olumidemicheal7@gmail.com>
2022-10-18 01:42:27 -0700

    took another approach

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
-import sys
+"""using a REST API, for a given employee ID, returns information about
+his/her TODO list progress"""
+import requests
+from sys import argv
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+
+if __name__ == "__main__":
+    """Your code should not be executed when imported"""
+
+    user_id = argv[1]
+
+    todos = requests.get(
+        "http://jsonplaceholder.typicode.com/todos?userId={}".format(
+            user_id))
+    user = requests.get(
+        "http://jsonplaceholder.typicode.com/users/{}".format(
+            user_id))
+
+    if len(user.json()):
+        total = 0
+        completed = 0
+        tasks_list = []
+        for tarea in todos.json():
+            total += 1
+            if tarea.get("completed") is True:
+                completed += 1
+                tasks_list.append(tarea.get("title"))
+
+        print(
+            "Employee {} is done with tasks({}/{}):".format(
+                user.json().get("name"), completed, total))
+        for task in tasks_list:
+            print("\t {}".format(task))
