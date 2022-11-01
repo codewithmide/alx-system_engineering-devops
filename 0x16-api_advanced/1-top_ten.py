c99fa875d9679adbe5d08bc8fb55b6911aec806d
Olumide Micheal <olumidemicheal7@gmail.com>
2022-11-01 10:32:08 -0700

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+Write a function that queries the Reddit API
+and prints the titles of the first 10 hot posts listed for a given subreddit.
 
 Requirements:
 
 If not a valid subreddit, print None.
-NOTE: Invalid subreddits may return a redirect to search results. Ensure that you are not following redirects.
+NOTE: Invalid subreddits may return a redirect to search results.
+Ensure that you are not following redirects.
 """
 import requests
 import json
