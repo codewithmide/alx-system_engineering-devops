00f8df7121f2e58af48da0f5caad1807e9265a85
Olumide Micheal <olumidemicheal7@gmail.com>
2022-11-01 10:25:59 -0700

    fix too long line

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
-Hint: No authentication is necessary for most features of the Reddit API. If you’re getting errors related to Too Many Requests, ensure you’re setting a custom User-Agent.
+Write a function that queries the Reddit API
+and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+
+Hint: No authentication is necessary for most features of the Reddit API.
+If you’re getting errors related to Too Many Requests,
+ensure you’re setting a custom User-Agent.
 
 Requirements:
 Prototype: def number_of_subscribers(subreddit)
-NOTE: Invalid subreddits may return a redirect to search results. Ensure that you are not following redirects.
+NOTE: Invalid subreddits may return a redirect to search results.
+Ensure that you are not following redirects.
 """
 import requests
 
